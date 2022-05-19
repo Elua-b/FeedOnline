09e08d106e3e22b37b3675d77b8601cc19652a58
Elua-b <bugingoeloi@gmail.com>
2022-05-19 06:05:27 +0200

    online-feed

---
+++ src/items.jsx
@@ -0,0 +1,183 @@
+const data=[
+    {
+        id:1,
+        head:"African",
+        image:"./african1.jpg",
+        price:"$1000",
+        content:"life is good",
+        
+    },
+    {
+        id:2,
+        head:"America",
+        image:"./american1.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:3,
+        head:"Japan",
+        image:"./japanese1.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:4,
+        head:"Thailand",
+        image:"./tailand1.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:5,
+        head:"Africa",
+        image:"./african2.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:6,
+        head:"American",
+        image:"./american2.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:7,
+        head:"Japanese",
+        image:"./japanese2.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:8,
+        head:"Thailand",
+        image:"./tailand2.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:9,
+        head:"Africa",
+        image:"./african3.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:10,
+        head:"Japan",
+        image:"./japanese3.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:11,
+        head:"Thailand",
+        image:"./tailand3.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:12,
+        head:"America",
+        image:"./American3.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:13,
+        head:"Africa",
+        image:"./african4.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:14,
+        head:"Thailand",
+        image:"./tailand4.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:15,
+        head:"Japan",
+        image:"./japanese4.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:16,
+        head:"America",
+        image:"./american4.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:17,
+        head:"Africa",
+        image:"./african5.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    ,
+    {
+        id:18,
+        head:"America",
+        image:"./american5.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+    // ,
+    // {
+    //     id:19,
+    //     head:"Thailand",
+    //     image:"./tailand5.jpg",
+    //     price:"$1030",
+    //     content:"life is good",
+        
+    // }
+    ,
+    {
+        id:20,
+        head:"Japan",
+        image:"./japanese5.jpg",
+        price:"$1030",
+        content:"life is good",
+        
+    }
+       
+    
+]
+export default data
